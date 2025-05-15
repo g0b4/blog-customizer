9cb42b8bdf9b0d2fec1cefdb1f0fc5e2da62cc82
Danil Korolev <korolev.spasi@yandex.ru>
2025-05-15 16:47:56 +0500

    Initial commit

--- src/ui/spacing/Spacing.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.module.scss';
-
-type SpacingProps = {
-	size: 4 | 50 | 24 | 196 | 90;
-};
-
-export const Spacing = ({ size = 4 }: SpacingProps) => {
-	return <div className={styles['spacing-h-' + size]} />;
-};
